dadbd50b8263c398207099e24e376d5d6bbb0153
Glen Langer <contao.modul@glen-langer.de>
2015-02-22 02:06:44 +0100

    Fixed #2, Paletteneintrag erweitert

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -33,6 +33,6 @@
   );
   
   $GLOBALS['TL_DCA']['tl_module']['palettes']['articlefilter'] = '{title_legend},name,headline,type;{config_legend},af_groups,af_defaultfilter;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-  $GLOBALS['TL_DCA']['tl_module']['palettes']['articlefilter_links'] = '{title_legend},name,headline,type;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+  $GLOBALS['TL_DCA']['tl_module']['palettes']['articlefilter_links'] = '{title_legend},name,headline,type;{config_legend},af_groups,af_defaultfilter;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
   $GLOBALS['TL_DCA']['tl_module']['palettes']['articlefilter_results'] = '{title_legend},name,headline,type;{config_legend},af_showfilter,af_sorting,perPage;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-?>
+
