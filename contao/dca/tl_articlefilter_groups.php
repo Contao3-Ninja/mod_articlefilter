add3eeaeb069faa49b1684f6e14ffa7331ed4586
Martin Kozianka <martin@kozianka.de>
2016-02-09 11:21:47 +0100

    editheader

--- contao/dca/tl_articlefilter_groups.php
+++ contao/dca/tl_articlefilter_groups.php
@@ -31,22 +31,29 @@ $GLOBALS['TL_DCA']['tl_articlefilter_groups'] = [
 			]
 		],
 		'operations' => [
+            'toggle' => [
+                'label'               => &$GLOBALS['TL_LANG']['tl_articlefilter_groups']['toggle'],
+                'icon'                => 'visible.gif',
+                'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+                'button_callback'     => ['tl_articlefilter_groups', 'toggleIcon']
+            ],
 			'edit' => [
 				'label'               => &$GLOBALS['TL_LANG']['tl_articlefilter_groups']['edit'],
 				'href'                => 'table=tl_articlefilter_criteria',
 				'icon'                => 'edit.gif'
 			],
+            'editheader' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_articlefilter_groups']['editheader'],
+                'href'                => 'act=edit',
+                'icon'                => 'header.gif',
+                'button_callback'     => array('tl_articlefilter_groups', 'editHeader')
+            ),
 			'delete' => [
 				'label'               => &$GLOBALS['TL_LANG']['tl_articlefilter_groups']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.gif',
 				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
-			],
-			'toggle' => [
-				'label'               => &$GLOBALS['TL_LANG']['tl_articlefilter_groups']['toggle'],
-				'icon'                => 'visible.gif',
-				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
-				'button_callback'     => ['tl_articlefilter_groups', 'toggleIcon']
 			],
 		]
 	],
@@ -87,6 +94,15 @@ $GLOBALS['TL_DCA']['tl_articlefilter_groups'] = [
 
 class tl_articlefilter_groups extends Backend
 {
+    public function editHeader($row, $href, $label, $title, $icon, $attributes)
+    {
+        if (!$this->User->canEditFieldsOf('tl_articlefilter_groups'))
+        {
+            return Image::getHtml(preg_replace('/\.gif$/i', '_.gif', $icon)).' ';
+        }
+        return '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ';
+    }
+
     public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
     {
         if (strlen(Input::get('tid')))
